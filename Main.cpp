fbb6d95b4b38f48d90935a08260d695e2228d461
kabirajk <kabiraj.mc19@bitsathy.ac.in>
2022-04-22 19:30:38 +0530

    clang-formatted

--- Main.cpp
+++ Main.cpp
@@ -1,49 +1,43 @@
+#include <fstream>
 #include <iostream>
 #include <vector>
-#include <fstream>
-std::vector<std::string>MOVES;
-std::vector<std::string>Completed_moves;
-void push_to_completed (std::string completedmove)
-    {
-        Completed_moves.push_back(completedmove);
-    }
-void EXIT()
-{
-    std::fstream logfile("log.txt",std::ios::app);
-    try
-    {
-        if(!logfile.is_open())
-            throw "There is no file";
-        logfile<<"\n";
-    }
-    catch(const char *c)
-    {   
-        std::ofstream logfile("log.txt",std::ios::app);
-    }
-    for(int i=0;i<Completed_moves.size();i++)
-    {
-        logfile<<Completed_moves[i]<<std::endl;
-    }
+std::vector<std::string> available_moves;
+std::vector<std::string> Completed_available_moves;
+void pushToCompleted(std::string completedmove) {
+  Completed_available_moves.push_back(completedmove);
+}
+void on_exit() {
+  std::fstream logfile("log.txt", std::ios::app);
+  try {
+    if (!logfile.is_open())
+      throw "There is no file";
+    logfile << "\n";
+  } catch (const char *c) {
+    std::ofstream logfile("log.txt", std::ios::app);
+  }
+  for (int i = 0; i < Completed_available_moves.size(); i++) {
+    logfile << Completed_available_moves[i] << std::endl;
+  }
 }
 
 using namespace std;
-#include"helper.h"
-void printv()
-{   std::cout<<"Here the available moves \n";
-    for(int i=0;i<MOVES.size();i++)
-    {
-        if (MOVES[i] != "*")
-            std::cout<<Box::numeric_to_Alphanumeric(Box::row(MOVES[i]),Box::col(MOVES[i]))<<" ";
-        else if(MOVES[i]=="*")
-            std::cout<<"\b"<<MOVES[i]<<"  ";
-    }
-    cout<<"\n";
+#include "HELPER.h"
+void printv() {
+  std::cout << "Here the available available_moves \n";
+  for (int i = 0; i < available_moves.size(); i++) {
+    if (available_moves[i] != "*")
+      std::cout << Box::numericToAlphaNumeric(Box::row(available_moves[i]),
+                                              Box::col(available_moves[i]))
+                << " ";
+    else if (available_moves[i] == "*")
+      std::cout << "\b" << available_moves[i] << "  ";
+  }
+  cout << "\n";
 }
 
-int main()
-{   
-    initializer();
-    printboard();
-    GameLoop();
-    return 0;
+int main() {
+  board_initializer();
+  print_board();
+  gameloop();
+  return 0;
 }
